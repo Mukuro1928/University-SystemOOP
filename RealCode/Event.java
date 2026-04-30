ff9980ccab573e3e3cc65ef6a3abde50101eac39
tepjey <athif194@yahoo.com>
2026-05-01 01:45:46 +0800

    Refactor Event class methods for clarity
    Updated method names for clarity and added a method to print event details.

--- RealCode/Event.java
+++ RealCode/Event.java
@@ -17,14 +17,17 @@ public class Event extends Club {
         this.attendeeCount = attendeeCount;
     }
     
-    public void updateEventDetails(String eventName, String eventDate, String eventLocation, int attendeeCount){
+    public void setEventDetails(String eventName, String eventDate, String eventLocation, int attendeeCount){
         this.eventName = eventName;
         this.eventDate = eventDate;
         this.eventLocation = eventLocation;
         this.attendeeCount = attendeeCount;
     }
-    
-    public String getEventSummary(){
-        return "Event: " + eventName + ", Date: " + eventDate + ", Location: " + eventLocation + ", Attendees: " + attendeeCount;
+
+    public void getEventDetails(){
+        System.out.println("Event Name: " + eventName);
+        System.out.println("Event Date: " + eventDate);
+        System.out.println("Event Location: " + eventLocation);
+        System.out.println("Attendee Count: " + attendeeCount);
     }
 }
