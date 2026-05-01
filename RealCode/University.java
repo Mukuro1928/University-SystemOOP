5514fd26d76f76812aa0bb8ecccafeea80d35f6c
nuzilynx <nuzil.efan@gmail.com>
2026-05-01 21:33:48 +0800

    Update University.java

--- RealCode/University.java
+++ RealCode/University.java
@@ -7,37 +7,44 @@
  *
  * @author nuzil
  */
-import java.util.ArrayList;
-
 public class University {
-    // Attributes as defined in the UML
+
     private String name;
     private String location;
-    private ArrayList<Club> clubs; // This implements the clubs[]: Club attribute
+    private ArrayList<Club> clubs;
 
-    // Constructor
+    // Constructor - Initialises attributes and the list
     public University(String name, String location) {
         this.name = name;
         this.location = location;
-        this.clubs = new ArrayList<>(); // Initialize the list
+        this.clubs = new ArrayList<>(); 
     }
 
-    // Method to add a club (Aggregation)
+    //Method to add a club to the university.
     public void addClub(Club club) {
         clubs.add(club);
         System.out.println("Club '" + club.getClubName() + "' has been added to " + this.name);
     }
 
-    // Method to display all clubs
+    //Method to display all clubs currently registered at the university.
     public void displayAllClubs() {
         System.out.println("\n--- Clubs at " + this.name + " (" + this.location + ") ---");
+        
         if (clubs.isEmpty()) {
             System.out.println("No clubs registered yet.");
         } else {
-            for (Club c : clubs) {
-                // This assumes the Club class has a getClubName() method
-                System.out.println("- " + c.getClubName());
+            for (int i = 0; i < clubs.size(); i++) {
+                Club currentClub = clubs.get(i); 
+                System.out.println("- " + currentClub.getClubName());
             }
         }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLocation() {
+        return location;
     }
 }
