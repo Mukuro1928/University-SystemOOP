195b653f606f7ddcd8eba642818099891d1ff225
Mukuro1928 <a.mukhlis123@gmail.com>
2026-05-01 21:16:04 +0800

    Refactor Student class for simplified attendance method
    Refactored Student class to simplify constructor and combine methods for course and event attendance.

--- Contoh/Student.java
+++ Contoh/Student.java
@@ -1,27 +1,15 @@
 package universitymanagementtest;
 
-/**
- * Author: [Member 3 Name]
- * Matric No: [Member 3 Number]
- */
 public class Student {
-    private String studentID;
     private String name;
 
-    public Student(String studentID, String name) {
-        this.studentID = studentID;
+    public Student(String name) {
         this.name = name;
     }
 
-    // Association: Passing a Course object as a parameter
-    public void enrollInCourse(Course course) {
-        System.out.println(name + " has enrolled in: " + course.getCourseName());
+    // Association: Passing objects as parameters to a method
+    public void attend(Course c, Event e) {
+        System.out.println(name + " is attending " + c.getCourseName() + 
+                           " and then going to " + e.getDetails());
     }
-
-    // Association: Passing an Event object as a parameter
-    public void registerForEvent(Event event) {
-        System.out.println(name + " is attending event: " + event.getEventDetails());
-    }
-
-    public String getName() { return name; }
 }
