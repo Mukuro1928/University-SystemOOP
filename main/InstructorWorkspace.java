5b92c007bc1cbcaf407e9a9ea4110ea5fc6a2644
Gucci664 <iqmalh991@gmail.com>
2026-06-08 01:58:55 +0800

    Update InstructorWorkspace.java

--- main/InstructorWorkspace.java
+++ main/InstructorWorkspace.java
@@ -10,7 +10,7 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.*;
 
 /**
- * Developed by Nuzil
+ * Developed by AMIR NUZIL IRFAN BIN AMIR ZAHARIMAN 2512533
  * InstructorWorkspace.java — Instructor Management Panel
  * ======================================================
  * Provides a graphical interface for managing instructor records.
